f63e74b6b3a9ce6ce3d91ac0ac9fcac76f5ccc8d
Grazielle <grazielle.anaia@gmail.com>
2024-12-13 10:28:04 -0500

    adicionando metodo que salva dados usuarios

--- src/main/java/com/grazielle/usuario/infrastructure/entity/Telefone.java
+++ src/main/java/com/grazielle/usuario/infrastructure/entity/Telefone.java
@@ -2,10 +2,7 @@ package com.grazielle.usuario.infrastructure.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -13,6 +10,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "telefone")
+@Builder
+
 
 public class Telefone {
 
@@ -20,36 +19,10 @@ public class Telefone {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "numero", length = 10)
+    @Column(name = "numero", length = 50)
     private String numero;
 
     @Column(name = "ddd", length = 3)
     private String ddd;
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Entity
-    @Table(name = "endereco")
-    public static class Endereco {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-
-        @Column(name = "rua")
-        private String rua;
-        @Column(name = "numero")
-        private Long numero;
-        @Column(name = "complemento", length = 10)
-        private String complemento;
-        @Column(name = "cidade", length = 150)
-        private String cidade;
-        @Column(name = "estado", length = 2)
-        private String estado;
-        @Column(name = "cep", length = 9)
-        private String cep;
-
-    }
 }
